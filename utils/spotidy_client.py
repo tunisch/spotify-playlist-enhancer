af243d7edb374bf97c2d64cc4ada217e65ab0fcd
Tunahan Erturk <tunaerturk1@gmail.com>
2025-03-13 23:49:23 +0300

    last chance.

--- utils/spotidy_client.py
+++ utils/spotidy_client.py
@@ -7,23 +7,13 @@ from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 
 # Spotify bağlantısı
-sp_oauth = SpotifyOAuth(
-    client_id=os.getenv('CLIENT_ID'),
-    client_secret=os.getenv('CLIENT_SECRET'),
-    redirect_uri=os.getenv('REDIRECT_URI'),
-    scope='playlist-modify-private playlist-read-private user-read-currently-playing user-read-playback-state user-library-read user-top-read'
-)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+    scope='playlist-modify-private playlist-read-private'
+))
 
-token_info = sp_oauth.get_cached_token()
-if not token_info:
-    print(f"Enter the URL you were redirected to: ")
-    redirect_url = input()
-    code = sp_oauth.parse_response_code(redirect_url)
-    token_info = sp_oauth.get_access_token(code=code)
-
-if token_info:
-    sp = spotipy.Spotify(auth=token_info['access_token'])
-    user = sp.current_user()
-    print(f"Spotify'a bağlandın: {user['display_name']}")
-else:
-    print("Yetkilendirme başarısız.")
+# Kullanıcı bilgilerini alalım
+user = sp.current_user()
+print(f"Spotify'a bağlandın: {user['display_name']}")
