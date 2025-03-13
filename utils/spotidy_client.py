2e92a6b56480f9af451780469f7d1a25da9840a0
Tunahan Erturk <tunaerturk1@gmail.com>
2025-03-13 20:11:07 +0300

    first commit.

---
+++ utils/spotidy_client.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Environment variables yükleme
+load_dotenv()
+
+# Spotify bağlantısı
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    redirect_uri=os.getenv('REDIRECT_URI'),
+    scope='playlist-modify-private playlist-read-private user-read-currently-playing user-read-playback-state'
+))
+
+# Kullanıcı bilgilerini alalım
+user = sp.current_user()
+print(f"Spotify'a bağlandın: {user['display_name']}")
+
