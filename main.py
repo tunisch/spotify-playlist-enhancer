cbb5146f1681bc5ecbb6dc5059dbc5f7c6f164f3
Tunahan Erturk <tunaerturk1@gmail.com>
2025-03-13 21:07:54 +0300

    last change.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from utils.spotidy_client import sp
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'utils')))
+
+from spotidy_client import sp
 
 def get_playlist_tracks(sp, playlist_id):
     results = sp.playlist_tracks(playlist_id)
@@ -11,23 +15,20 @@ def get_playlist_tracks(sp, playlist_id):
 def find_similar_tracks(sp, track_id):
     features = sp.audio_features([track_id])[0]
     query = f"genre:{features['key']} tempo:{features['tempo']} energy:{features['energy']} danceability:{features['danceability']}"
-    
+
     results = sp.search(q=query, type='track', limit=10)
     similar_tracks = []
     for item in results['tracks']['items']:
         similar_tracks.append(item)
     return similar_tracks
 
-# Çalma listesi ID'sini buraya ekle
-playlist_id = 'https://open.spotify.com/playlist/0SYZIeRqDQGsbCvWkdoFLD?si=d44ba36b1f124176'
+playlist_id = 'ÇALMA_LISTESI_ID'
 
-# Çalma listesindeki parçaları al
 tracks = get_playlist_tracks(sp, playlist_id)
 for idx, track in enumerate(tracks):
     print(f"{idx + 1} {track['name']} - {track['artists'][0]['name']}")
 
-# Örnek şarkıya benzer şarkıları bul
-track_id = tracks[0]['id']  # Çalma listesinden bir şarkının ID'sini al
+track_id = tracks[0]['id']
 similar_tracks = find_similar_tracks(sp, track_id)
 for idx, track in enumerate(similar_tracks):
     print(f"Benzer {idx + 1} {track['name']} - {track['artists'][0]['name']}")
